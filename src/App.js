ee3747983152946513e1a17e25d1f33b27d4c947
Patrick Roberts <Patrick-M-Roberts@protonmail.com>
2024-04-15 18:16:27 -0500

    Learning Started
    Started following reacts tutorial.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+
+function MyButton(){
+  return(
+    <button>Click me</button>
+  );
+}
+
+function AboutMe(){
+  return(
+    <>
+    <h1>{user.name}</h1>
+    <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +49,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <AboutMe />
+      <MyButton />
     </div>
   );
 }
